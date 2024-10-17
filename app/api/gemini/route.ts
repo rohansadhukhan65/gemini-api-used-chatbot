6de45d726c5b6c8b8709f3aee5d6c30b8a1b835e
Rohan Sadhukhan <68492239+rohansadhukhan65@users.noreply.github.com>
2024-10-17 19:58:05 +0530

    init

---
+++ app/api/gemini/route.ts
@@ -0,0 +1,18 @@
+import { GEM_API } from "@/data/key";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export const POST = async (req: any) => {
+  try {
+    const { prompt } = await req.json();
+    const genAI = new GoogleGenerativeAI(GEM_API);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    const { response }: any = await model.generateContent(prompt);
+
+    return Response.json({
+      response: response?.candidates[0]?.content?.parts[0],
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ data/key.ts
@@ -0,0 +1,1 @@
+export const GEM_API = 'AIzaSyBxUFrHG6f_XBwOoX77JnsTgsj8ZkoGwfQ';
